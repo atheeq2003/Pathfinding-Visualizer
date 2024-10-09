422bc989b9a0bbf4b8ec76ef3e5be93b1b399868
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-10-09 13:24:22 +0530

    createWall

--- src/lib/algorithms/maze/binaryTree.ts
+++ src/lib/algorithms/maze/binaryTree.ts
@@ -1,7 +1,10 @@
-export const binaryTree = async {
+import {GridType, SpeedType } from "../../../utils/types";
+export const binaryTree = async (
     grid: GridType,
     startTile: TileType,
     endTile: TileType,
     setIsDisabled: (isDisabled: boolean) => void,
     speed: SpeedType
+) => {
+    createWall()
 }
