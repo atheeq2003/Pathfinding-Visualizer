ab4e31480ca1803ebd653067b01a7ce62520aa95
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-10-09 22:35:09 +0530

    dfs

--- src/utils/runPathfindingAlgorithm.ts
+++ src/utils/runPathfindingAlgorithm.ts
@@ -1,4 +1,5 @@
 import { bfs } from "../lib/algorithms/pathfinding/bfs";
+import { dfs } from "../lib/algorithms/pathfinding/dfs";
 import { AlgorithmType, GridType, TileType } from "./types";
 export const runPathfindingAlgorithm = ({
     algorithm,
@@ -14,6 +15,8 @@ export const runPathfindingAlgorithm = ({
     switch(algorithm) {
         case "BFS":
             return bfs(grid, startTile, endTile);
+        case "DFS":
+            return dfs(grid, startTile, endTile);
         default:
             return bfs(grid, startTile, endTile);
     }
